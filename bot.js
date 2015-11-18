504c58c714be1da994bbcce7704f68c2f4ee9120
ugkrut <timroyaljr@gmail.com>
2015-11-18 12:18:17 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,7 +94,7 @@ function respond() {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
     var rep = req.replace(/ /,"+");
-    postMessage("https://www.youtube.com/results?search_query="+rep+"");
+    postMessage("https://www.youtube.com/results?search_query="+request.text.substring(8,request.text.length));
     this.res.end();
   
   }
